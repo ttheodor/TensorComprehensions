c80ff05bd132d0812445ce8d0cd9ca632c501ef7
Theodoros Theodoridis <theodoridisgr@gmail.com>
2018-05-03 11:19:03 +0200

    Switch to group convo

--- test/test_other.cc
+++ test/test_other.cc
@@ -30,14 +30,27 @@ using namespace tc::polyhedral;
 using namespace tc::polyhedral::detail;
 
 namespace {
-DEFINE_uint64(N, 2048, "");
-DEFINE_uint64(M, 1024, "");
-DEFINE_uint64(T0, 1, "");
-DEFINE_uint64(T1, 1, "");
+DEFINE_uint64(N, 32, "");
+DEFINE_uint64(G, 32, "");
+DEFINE_uint64(C, 4, "");
+DEFINE_uint64(F, 4, "");
+DEFINE_uint64(W, 56, "");
+DEFINE_uint64(H, 56, "");
+DEFINE_uint64(KW, 3, "");
+DEFINE_uint64(KH, 3, "");
+DEFINE_uint64(T0, 0, "");
+DEFINE_uint64(T1, 0, "");
+DEFINE_uint64(T2, 0, "");
+DEFINE_uint64(T3, 0, "");
+DEFINE_uint64(T4, 0, "");
+DEFINE_uint64(T5, 0, "");
+DEFINE_uint64(T6, 0, "");
 DEFINE_uint64(B0, 1, "");
 DEFINE_uint64(B1, 1, "");
+DEFINE_uint64(B2, 1, "");
 DEFINE_uint64(G0, 1, "");
 DEFINE_uint64(G1, 1, "");
+DEFINE_uint64(G2, 1, "");
 } // namespace
 
 int main(int argc, char** argv) {
@@ -45,8 +58,12 @@ int main(int argc, char** argv) {
   ::google::InitGoogleLogging(argv[0]);
 
   string tc = R"TC(
-def fun(float(N, M) A, float(N, M) B) -> (C) {
-    C(n, m) = A(n, m) + B(n, m)
+def group_convolution(float(N,G,C,H,W) I, float(G,F,C,KH,KW) W1, float(G,F) B)
+-> (O)
+{
+    O(n, g, f, h, w) +=!
+        I(n, g, r_c, h + r_kh, w + r_kw) * W1(g, f, r_c, r_kh, r_kw)
+    O(n, g, f, h, w)  = O(n, g, f, h, w) + B(g, f)
 }
 )TC";
   lang::Parser p(tc);
@@ -59,17 +76,33 @@ def fun(float(N, M) A, float(N, M) B) -> (C) {
   auto scopTmp = polyhedral::Scop::makeScop(
       isl::with_exceptions::globalIslCtx(), halideComponents);
 
-  auto context = scopTmp->makeContext(
-      std::unordered_map<std::string, int>{{"N", FLAGS_N}, {"M", FLAGS_M}});
+  // auto context = scopTmp->makeContext(
+  // std::unordered_map<std::string, int>{{"N", FLAGS_N},
+  //{"G", FLAGS_G},
+  //{"C", FLAGS_C},
+  //{"F", FLAGS_F},
+  //{"W", FLAGS_W},
+  //{"H", FLAGS_H},
+  //{"KW", FLAGS_KW},
+  //{"KH", FLAGS_KH}});
+  auto context = scopTmp->makeContext(std::unordered_map<std::string, int>{});
 
   scopTmp = polyhedral::Scop::makeSpecializedScop(
       *scopTmp, context.intersect(scopTmp->globalParameterContext));
 
   auto opts = CudaMappingOptions::makePointwiseCudaMappingOptions()
-                  .tile(FLAGS_T0, FLAGS_T1)
-                  .mapToThreads({FLAGS_B0, FLAGS_B1})
-                  .mapToBlocks({FLAGS_G0, FLAGS_G1})
-                  .unroll(0);
+                  .tile(
+                      FLAGS_T0,
+                      FLAGS_T1,
+                      FLAGS_T2,
+                      FLAGS_T3,
+                      FLAGS_T4,
+                      FLAGS_T5,
+                      FLAGS_T6)
+                  .mapToThreads({FLAGS_B0, FLAGS_B1, FLAGS_B2})
+                  .mapToBlocks({FLAGS_G0, FLAGS_G1, FLAGS_G2})
+                  .unroll(0)
+                  .useSharedMemory(true);
   auto mappedScop =
       polyhedral::MappedScop::makeWithOuterBlockInnerThreadStrategy(
           std::move(scopTmp), opts);
