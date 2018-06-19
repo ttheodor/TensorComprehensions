4aeb0babb612e33bd5c1271f44835f29d5bf1471
Theodoros Theodoridis <theodoridisgr@gmail.com>
2018-06-19 14:59:52 +0200

    Fix shared memory limit

--- tc/core/polyhedral/cuda/mapped_scop.cc
+++ tc/core/polyhedral/cuda/mapped_scop.cc
@@ -1023,7 +1023,7 @@ std::unique_ptr<MappedScop> MappedScop::makeWithOuterBlockInnerThreadStrategy(
   if (cudaOptions.proto().use_shared_memory()) {
     size_t sharedMemorySize = cudaOptions.proto().has_max_shared_memory()
         ? cudaOptions.proto().max_shared_memory()
-        : querySharedMemorySize();
+        : 49152;
     // If reductions found, their synchronization requires an opaque cache in
     // shared memory.  Subtract 4k from available shared memory for each
     // reduction found, this is hack based on each thread of max 1024 in the
